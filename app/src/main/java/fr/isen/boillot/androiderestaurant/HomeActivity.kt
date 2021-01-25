44c61886771598e077be9d461744df921cf23647
Mathias <mathias.boillot@isen.yncrea.fr>
2021-01-25 13:41:54 +0100

    create and link categories to home page

--- app/src/main/java/fr/isen/boillot/androiderestaurant/HomeActivity.kt
+++ app/src/main/java/fr/isen/boillot/androiderestaurant/HomeActivity.kt
@@ -1,11 +1,52 @@
 package fr.isen.boillot.androiderestaurant
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import fr.isen.boillot.androiderestaurant.databinding.ActivityHomeBinding
+
+private lateinit var binding: ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        var optionSelected: Button;
+        binding.buttonStarter.setOnClickListener {
+            Toast.makeText(applicationContext, "Choisissez votre entrée", Toast.LENGTH_LONG).show()
+            val intent = Intent(this@HomeActivity, ListActivity::class.java)
+            optionSelected = findViewById(R.id.button_starter)
+            val message = optionSelected.text.toString()
+            intent.putExtra("message_key", message)
+            startActivity(intent)
+        }
+        binding.buttonMain.setOnClickListener {
+            Toast.makeText(applicationContext, "Choisissez votre plat", Toast.LENGTH_LONG).show()
+            val intent = Intent(this@HomeActivity, ListActivity::class.java)
+            optionSelected = findViewById(R.id.button_main)
+            val message = optionSelected.text.toString()
+            intent.putExtra("message_key", message)
+            startActivity(intent)
+        }
+        binding.buttonDessert.setOnClickListener {
+            Toast.makeText(applicationContext, "Choisissez votre dessert", Toast.LENGTH_LONG).show()
+            val intent = Intent(this@HomeActivity, ListActivity::class.java)
+            optionSelected = findViewById(R.id.button_dessert)
+            val category = optionSelected.text.toString()
+            intent.putExtra("category_key", category)
+            startActivity(intent)
+        }
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.i("HomeActivity", "onDestroy Called")
+    }
+
 }
---
+++ app/src/main/java/fr/isen/boillot/androiderestaurant/ListActivity.kt
@@ -0,0 +1,15 @@
+package fr.isen.boillot.androiderestaurant
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class ListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list)
+        val message = intent.getStringExtra("category_key")
+        val messageTextView: TextView = findViewById(R.id.textView)
+        messageTextView.text = message
+    }
+}
