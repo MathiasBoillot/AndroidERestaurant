afbbc8e9f613bb2d70dcdea8cd9a8f52e3f5d832
Mathias <mathias.boillot@isen.yncrea.fr>
2021-02-02 12:12:56 +0100

    SignIn XML

--- app/src/main/java/fr/isen/boillot/androiderestaurant/SignInActivity.kt
+++ app/src/main/java/fr/isen/boillot/androiderestaurant/SignInActivity.kt
@@ -1,44 +1,11 @@
 package fr.isen.boillot.androiderestaurant
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.android.volley.Request
-import com.android.volley.toolbox.*
-import com.google.gson.Gson
-import fr.isen.boillot.androiderestaurant.databinding.ActivitySignInBinding
-import fr.isen.boillot.androiderestaurant.login.LoginFormData
-import fr.isen.boillot.androiderestaurant.login.LoginViewModel
-import org.json.JSONObject
+import android.os.Bundle
 
 class SignInActivity : AppCompatActivity() {
-    private lateinit var binding: ActivitySignInBinding
-    private lateinit var loginViewModel: LoginViewModel
-    private lateinit var dataForm: LoginFormData
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySignInBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        loginViewModel = LoginViewModel()
-
-        binding.login.setOnClickListener {
-
-        }
+        setContentView(R.layout.activity_sign_in)
     }
-
-    override fun onUserInteraction() {
-        super.onUserInteraction()
-        loginViewModel = LoginViewModel()
-        dataForm = LoginFormData(
-            email = binding.registerEmail.text.toString(),
-            firstName = binding.registerFirstname.text.toString(),
-            lastName = binding.registerLastname.text.toString(),
-            address = binding.registerAddress.text.toString(),
-            password = binding.registerPassword.text.toString()
-        )
-
-        binding.login.isEnabled = loginViewModel.loginDataChanged(dataForm)
-    }
-
-
 }
